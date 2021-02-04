4ab6fb88c56dd09ec9fcf2d1465f5331a7f9ea1c
ZKAW <zkaw.exod@gmail.com>
2021-02-04 11:38:02 +0100

    Add reset btn

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ class Main {
     constructor() {
         this.els = {
             addBtn: document.querySelector('.addBtn'),
+            resetBtn: document.querySelector('.resetBtn'),
             numberField: document.querySelector('#numberField'),
             coefField: document.querySelector('#coefField'),
-            infoLabel: document.querySelector('.infoLabel')
+            infoLabel: document.querySelector('.infoLabel'),
+            resultLabel: document.querySelector('.resultLabel')
         }
         
         this.total = 0,
@@ -18,7 +20,11 @@ class Main {
         // Btn click listener
         this.els.addBtn.addEventListener('click', () => {
             this.getInput()
-        })   
+        })
+
+        this.els.resetBtn.addEventListener('click', () => {
+            this.resetInput()
+        })
 
         // Key listener
         document.addEventListener("keydown", event => {
@@ -32,27 +38,43 @@ class Main {
         let nbrAwr = this.els.numberField.value.replace(',','.')
         let coefAwr = this.els.coefField.value.replace(',','.')
 
-        this.els.numberField.value = ''
-        this.els.coefField.value = ''
-        this.els.numberField.focus()
-
-
-        if (coefAwr.length < 1 || coefAwr == undefined || coefAwr == null) coefAwr = '1'
-        
         if (nbrAwr.length > 0) {
+            this.els.numberField.value = ''
+            this.els.coefField.value = ''
+            this.els.numberField.focus()
+
+            if (coefAwr.length < 1 || coefAwr == undefined || coefAwr == null) coefAwr = '1'
+        
             if (!this.floatValidator.test(nbrAwr) || !this.floatValidator.test(coefAwr)) {
                 this.els.infoLabel.innerHTML = 'Veuillez entrer uniquement des nombres entiers ou à virgule positifs.'
                 return
     
             } else {
-                // this.els.infoLabel.innerHTML = `\nLa note ${nbrAwr} coef x${coefAwr} a été ajoutée avec succès !`
+                this.els.infoLabel.innerHTML = `La note ${nbrAwr} coef x${coefAwr} a été ajoutée avec succès !`
                 this.addGrade(parseFloat(nbrAwr), parseFloat(coefAwr))
+                this.els.resetBtn.classList.remove('nodisplay')
+                this.els.resetBtn.style.transition = '';
                 this.finishRequest(this.total, this.dividend)
                 return
             }
+        } else {
+            this.els.infoLabel.innerHTML = 'Veuillez remplir au moins la case "Note"'
+            return
         }
-
     }
+
+    resetInput() {
+        this.els.resetBtn.style.transition = 'none';
+        this.els.resetBtn.classList.add('nodisplay')
+        this.els.numberField.value = ''
+        this.els.coefField.value = ''
+        this.total = 0
+        this.dividend = 0
+        this.els.infoLabel.innerHTML = ''
+        this.els.resultLabel.innerHTML = ''
+        this.els.numberField.focus()
+    }
+    
 
     calcMoy(total, dividend) {
         const moy = (total/dividend)
@@ -72,10 +94,10 @@ class Main {
             let finalGrade = this.calcMoy(total, dividend)
             finalGrade = Number((finalGrade).toFixed(2));
 
-            this.els.infoLabel.innerHTML = `\nVotre moyenne est de ${finalGrade}.`
+            this.els.resultLabel.innerHTML = `Votre moyenne est de ${finalGrade}.`
             return
         } else {
-            this.els.infoLabel.innerHTML = '\nVeuillez entrer au minimum une note avec un coef supérieur à 0.'
+            this.els.resultLabel.innerHTML = 'Veuillez entrer au minimum une note avec un coef supérieur à 0.'
             return
         }
     }
